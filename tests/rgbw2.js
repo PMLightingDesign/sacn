791f6a06cfe12d77075c7edbcefd2ebf731f3562
Peter Milo <pete.a.milo@gmail.com>
2023-03-09 13:00:32 -0500

    adjust test

--- tests/rgbw2.js
+++ tests/rgbw2.js
@@ -209,7 +209,7 @@ console.log(color);
 let h = 0;
 
 let sacn = new sACNSocket({
-  universes: [41,42,43,44,45,46,46,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64],
+  universes: [41,42,43,44,45,46,46,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74],
   priorities: [100, 100, 100, 100, 100, 100, 100, 100],
   interface: 'auto'
 });
@@ -251,7 +251,17 @@ sacn.on('ready', () => {
       61: fillUniverse(color),
       62: fillUniverse(color),
       63: fillUniverse(color),
-      64: fillUniverse(color)
+      64: fillUniverse(color),
+      65: fillUniverse(color),
+      66: fillUniverse(color),
+      67: fillUniverse(color),
+      68: fillUniverse(color),
+      69: fillUniverse(color),
+      70: fillUniverse(color),
+      71: fillUniverse(color),
+      72: fillUniverse(color),
+      73: fillUniverse(color),
+      74: fillUniverse(color)
     });
     // console.log(sacn.u[1].packet.output.slice(126, 129));
     sacn.send();
